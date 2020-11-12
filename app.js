14a8d2aa6dccd9b1a3d694497a0b30c5618b0150
Toyin Dawudu <toyindawudu@gmail.com>
2020-11-12 23:42:50 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  //Select the elements in the DOM
+ const grid  = document.querySelector('.grid'); 
+ let squares = document.querySelectorAll('.grid div');
+ const scoreDisplay = document.querySelector('#score');
+ const startButton = document.querySelector('#start-btn');
+ 
+ const width = 0;
+ const height = 0;
+
+}); // This fires of this line of code when the page is loading
