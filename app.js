92b3b1476ecc6770054dc3ddf533ed76846b25cf
Toyin Dawudu <toyindawudu@gmail.com>
2020-11-16 21:42:37 +0000

    Created the array list for  all the Tetromino shapes

--- app.js
+++ app.js
@@ -1,11 +1,62 @@
 document.addEventListener('DOMContentLoaded', () => {
-  //Select the elements in the DOM
+
+  // Select the elements in the DOM
  const grid  = document.querySelector('.grid'); 
  let squares = document.querySelectorAll('.grid div');
  const scoreDisplay = document.querySelector('#score');
- const startButton = document.querySelector('#start-btn');
+ const startBtn = document.querySelector('#start-btn');
  
- const width = 0;
+ const width = 10;
  const height = 0;
 
-}); // This fires of this line of code when the page is loading
+
+// The Tetrominoes
+
+const lTetromino = [
+  [1, width+1, width*2+1, 2],
+  [width, width+1, width+2, width*2+2],
+  [1, width+1, width*2+1, width*2],
+  [width, width*2, width*2+1, width*2+2]
+]
+
+const zTetromino = [
+  [0, width, width+1, width*2+1],
+  [width+1, width+2, width*2, width*2+1],
+  [0, width, width+1, width*2+1],
+  [width+1, width+2, width*2, width*2+1]
+]
+
+const tTetromino = [
+  [1, width, width+1, width+2],
+  [1, width+1, width+2, width*2+1],
+  [width, width+1, width+2, width*2+1],
+  [1, width, width+1, width*2+1]
+]
+
+const oTetromino = [
+  [0,1,width, width+1],
+  [0,1,width, width+1],
+  [0,1,width, width+1],
+  [0,1,width, width+1]
+]
+
+const iTetromino = [
+  [1,width+1,width*2+1,width*3+1],
+  [width,width+1,width+2,width+3],
+  [1,width+1,width*2+1,width*3+1],
+  [width,width+1,width+2,width+3]
+]
+
+
+const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+// Establish the starting position of the Tetrominoes
+
+// Assign a Tetromino shape to a variable
+
+
+// Create a function that draws the first rotation in the first tetromino
+
+
+
+}); // This fires of this line of code when the page is loading 
